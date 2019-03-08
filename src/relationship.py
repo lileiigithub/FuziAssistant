c4184d0b780b674482bf2d121e324d9be04239bc
lucklee <lilei_mymail@foxmail.com>
2019-03-09 00:03:55 +0800

    add xmid
    1. xmid

--- src/relationship.py
+++ src/relationship.py
@@ -9,11 +9,10 @@ class Person(object):
         self.mate = None
 
 class MatchPerson(object):
-    def __init__(self):
-        self.youngerName = None
-        self.relationUp = None
-        self.elderName = None
-        self.relationDown = None
+    def __init__(self, _younger_person_obj, _older_person_obj, _relation):
+        self.younger = _younger_person_obj
+        self.relation = _relation
+        self.elderName = _older_person_obj
 
     def saveToDatabase(self):
         pass
