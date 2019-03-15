f915cc4896ec54e712a8eb287dec05bd7570b718
lucklee <lilei_mymail@foxmail.com>
2019-03-16 00:03:04 +0800

    add log
    .

--- src/relationship.py
+++ src/relationship.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from fuziLog import FzLog
 
 class Person(object):
+
     def __init__(self):
         self.name = None
         self.gender = None
@@ -8,13 +10,16 @@ class Person(object):
         self.mother = None
         self.mate = None
 
+
 class MatchPerson(object):
+
     def __init__(self, _younger_person_obj, _older_person_obj, _relation):
         self.younger = _younger_person_obj
         self.relation = _relation
         self.elderName = _older_person_obj
 
     def saveToDatabase(self):
+
         pass
 
 
