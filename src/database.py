0b839db9798199cc465eb2f1841218367433ed9b
lucklee <lilei_mymail@foxmail.com>
2019-03-17 23:52:05 +0800

    change a lot
    change a lot

--- src/database.py
+++ src/database.py
@@ -1,7 +1,58 @@
 # -*- coding: utf-8 -*-
 from fuziLog import FzLog
+import sqlite3
+import os
+from datetime import datetime
 
 class Database(object):
-
+    database_file = 'fuzi.db'
     def __init__(self):
+        if os.path.exists(Database.database_file)==False:
+            self.init_database()
+        # self.c = self.conn.cursor()
+
+    def init_database(self):
+        conn = sqlite3.connect(Database.database_file)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE FAMILY_INFO
+               (ID INT PRIMARY KEY         NOT NULL,
+                DATETIME     TEXT         NOT NULL,
+               FAMILY_INDEX      INT2         NOT NULL,
+               YOUNG_NAME     TEXT         NOT NULL,
+               YOUNG_GENDER   BOOLEAN      NOT NULL,
+               RELATION       CHARACTER(10)  NOT NULL,
+               OLD_NAME       TEXT         NOT NULL,
+               OLD_GENDER     BOOLEAN      NOT NULL);''')
+        conn.commit()
+        conn.close()
+        FzLog.info("Create database fuzi.db successfully")
+
+    @staticmethod
+    def insert_family_info(_id,_family_index,_young_name,_young_gender,
+                    _relation,_old_name,_old_gender):
+        conn = sqlite3.connect(Database.database_file)
+        c = conn.cursor()
+        data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        COMMAND = '''INSERT INTO FAMILY_INFO(ID,DATETIME,FAMILY_INDEX,YOUNG_NAME,YOUNG_GENDER,RELATION,OLD_NAME,OLD_GENDER)
+         VALUES('''+ Database._str(_id)+Database._str(data_time)+Database._str(_family_index)+Database._str(_young_name)\
+                  + Database._str(_young_gender)+ Database._str(_relation)+ Database._str(_old_name)+ Database._str(_old_gender,"")+");"
+        try:
+            c.execute(COMMAND)
+            conn.commit()
+        except Exception as e: # 发生错误
+            FzLog.error("Insert COMMAND:%s ERROR: %s", COMMAND, e)
+        else:
+            FzLog.info("Success Insert an information: %s", COMMAND)
+        conn.close()
+
+    @staticmethod
+    def _str(_value, _comma = ","):
+        return "'"+str(_value)+"'"+_comma
+
+    @staticmethod
+    def last_id():
         pass
+
+if __name__ == '__main__':
+    db = Database()
+    db.insert_family_info(1,1,"赵",1,"爷爷","钱",1)
--- src/relationship.py
+++ src/relationship.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fuziLog import FzLog
 import argparse
+from database import Database
 
 class Person(object):
 
@@ -11,17 +12,19 @@ class Person(object):
         # self.mother = None
         # self.mate = None
 
-
 class Relationship(object):
     pass
 
-class MatchPerson(object):
+class FamilyInfo(object):
+    def __init__(self,_family_index):
+        self.family_index = _family_index
+        self.id = Database.last_id()+1
+        # self.younger = _younger_person_obj
+        # self.relation = _relation
+        # self.elderName = _older_person_obj
 
-    def __init__(self, _younger_person_obj, _older_person_obj, _relation):
-        self.younger = _younger_person_obj
-        self.relation = _relation
-        self.elderName = _older_person_obj
-
+    def insert_family_info(self,_young_name,_young_gender,_relation,_old_name,_old_gender):
+        Database.insert_family_info(self.id,self.family_index,_young_name,_young_gender,_relation,_old_name,_old_gender)
 
     def savePersonInfo(self):
         pass
@@ -35,9 +38,6 @@ class MatchPerson(object):
 class Demand(object):
     pass
 
-
-class FamilyInfo(object):
-    pass
 
 
 
