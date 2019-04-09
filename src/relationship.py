142aed8e36875952e6e335939c113a85a0842f92
lucklee <lilei_mymail@foxmail.com>
2019-04-09 21:54:12 +0800

    window

--- src/relationship.py
+++ src/relationship.py
@@ -15,16 +15,14 @@ class Person(object):
 class Relationship(object):
     pass
 
-class FamilyInfo(object):
+class AFamily(object):
+    # class about a family
     def __init__(self,_family_index):
         self.family_index = _family_index
-        self.id = Database.last_id()+1
-        # self.younger = _younger_person_obj
-        # self.relation = _relation
-        # self.elderName = _older_person_obj
 
     def insert_family_info(self,_young_name,_young_gender,_relation,_old_name,_old_gender):
-        Database.insert_family_info(self.id,self.family_index,_young_name,_young_gender,_relation,_old_name,_old_gender)
+        id = Database.last_id() + 1
+        Database.insert_family_info(id,self.family_index,_young_name,_young_gender,_relation,_old_name,_old_gender)
 
     def savePersonInfo(self):
         pass
